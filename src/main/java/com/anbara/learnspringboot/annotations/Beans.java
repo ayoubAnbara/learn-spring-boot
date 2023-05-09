ee2941a702dd59456c7e6e0eaaa89b173d36cc0c
Ayoub Anbara <anbara.ayoub@gmail.com>
2023-05-09 17:24:15 +0100

    Explore Spring boot 3 Features

--- src/main/java/com/anbara/learnspringboot/annotations/Beans.java
+++ src/main/java/com/anbara/learnspringboot/annotations/Beans.java
@@ -1,6 +1,5 @@
 package com.anbara.learnspringboot.annotations;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
  * @author Ayoub ANBARA
  */
 @Configuration
-@Slf4j
+
 public class Beans {
 
     /**
